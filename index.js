68c13d0f9d29604f77a9fbce2a22b7603d8163fc
Visrozar <elvisrozarioefr@gmail.com>
2018-02-06 14:53:58 +0530

    created user model and user GET and POST requests

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
+const router = express.Router();
 var app = express();
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const users = require('./routes/users')(router);
+const bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err)=> {
@@ -11,6 +14,14 @@ mongoose.connect(config.uri, (err)=> {
         console.log("Connected to Database");
     }
 });
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}))
+
+//parse application/json
+app.use(bodyParser.json());
+
+app.use('/users', users);
 
 app.get('/', function(req, res){
   res.send('hello Elvis');
