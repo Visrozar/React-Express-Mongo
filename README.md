b6020e0ae3ccb1cdd72c267ecf7481bedf94c68b
Visrozar <elvisrozarioefr@gmail.com>
2018-02-06 13:52:31 +0530

    installed mongoDb

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# React-Express-Mongo" 
+
+## Installation Instructions
+
+*	run npm install
+*	install and setup mongoDB
+*	run mongod (make sure it's running on port 27017, which is the default port, else make changes to config accordingly)
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err)=> {
+    if (err) {
+        console.log("Could not connect to Database", err);
+    } else {
+        console.log("Connected to Database");
+    }
+});
 
 app.get('/', function(req, res){
   res.send('hello Elvis');
